56d4edb35e0c792bf13e5c9b8d5536bd6b5778b6
Petar Gerzilov <immortallfresh@abv.bg>
2018-10-21 22:56:41 +0300

    Included t1 t2 & t3 from week 3

---
+++ PUIntroHomeworks/BusTicketLuckyNumbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusTicketLuckyNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int luckyTickets = 0;
+            int unluckyTickets = 0;
+
+            
+            for (int i = 0; i < 1000000; i++)
+            {
+                if ((i / 100000 + i % 100000 / 10000 + i % 100000 % 10000 / 1000) == (i % 100000 % 10000 % 1000 / 100 + i % 100000 % 10000 % 1000 % 100 / 10 + i % 10))
+                {
+                    Console.WriteLine(i);
+                    luckyTickets++;
+                }
+                else
+                {
+                    unluckyTickets++;
+                }
+            }
+
+            Console.WriteLine("The number of lucky tickets is: " + luckyTickets);
+            Console.WriteLine("The number of unlucky tickets is: " + unluckyTickets);
+            Console.WriteLine($"Total tickets: {luckyTickets + unluckyTickets}");
+        }
+    }
+}
---
+++ PUIntroHomeworks/SimpleDeckOfCards/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleDeckOfCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Do you want to see a full deck of cards? (yes or no)");
+            string answer = Console.ReadLine();
+
+            while (answer != "yes")
+            {
+                Console.WriteLine("Do you want to see a full deck of cards? (yes or no)");
+                answer = Console.ReadLine();
+            }
+
+            Console.WriteLine($"A ♥, A ♦, A ♣, A ♠");
+            Console.WriteLine($"2 ♥, 2 ♦, 2 ♣, 2 ♠");
+            Console.WriteLine($"3 ♥, 3 ♦, 3 ♣, 3 ♠");
+            Console.WriteLine($"4 ♥, 4 ♦, 4 ♣, 4 ♠");
+            Console.WriteLine($"5 ♥, 5 ♦, 5 ♣, 5 ♠");
+            Console.WriteLine($"6 ♥, 6 ♦, 6 ♣, 6 ♠");
+            Console.WriteLine($"7 ♥, 7 ♦, 7 ♣, 7 ♠");
+            Console.WriteLine($"8 ♥, 8 ♦, 8 ♣, 8 ♠");
+            Console.WriteLine($"9 ♥, 9 ♦, 9 ♣, 9 ♠");
+            Console.WriteLine($"10 ♥, 10 ♦, 10 ♣, 10 ♠");
+            Console.WriteLine($"J ♥, J ♦, J ♣, J ♠");
+            Console.WriteLine($"Q ♥, Q ♦, Q ♣, Q ♠");
+            Console.WriteLine($"K ♥, K ♦, K ♣, K ♠");
+        }
+
+    }
+}
