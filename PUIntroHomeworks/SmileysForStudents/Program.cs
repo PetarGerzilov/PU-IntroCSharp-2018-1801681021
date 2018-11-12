008aaa313d2e219e0c361eb313bf457cba5d7e7a
Petar Gerzilov <immortallfresh@abv.bg>
2018-11-12 21:17:51 +0200

    includes t1,t2,t3 from homework week 6

---
+++ PUIntroHomeworks/SmileysForStudents/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SmileysForStudents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] student = System.IO.File.ReadAllLines(@"C:\Users\PESHO\Desktop\STD2018-1b-PetarGerzilov-1801681021\Students.txt");
+            int studentSum = 0;
+            int smiley = '☻';
+            System.Console.WriteLine("Contents of WriteLines2.txt = ");
+            foreach (string line in student)
+            {
+                student = line.Split(' ');
+                string firstName = student[1];
+                string fathersName = student[2];
+                string lastName = student[3];
+                string fullName = $"{firstName} {fathersName} {lastName}";
+
+                for (int i = 0; i < fullName.Length; i++)
+                {
+                    studentSum = studentSum + (int)fullName[i];
+                }
+                if (studentSum > 15000)
+                    Console.WriteLine($" {fullName} {(char)smiley} => {studentSum}");
+            }
+        }
+    }
+}
